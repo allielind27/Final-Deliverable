4b84809e1aa076bf3c53ca340f5f2cf9c7c465b9
allielind27 <allie.lind@du.edu>
2025-06-04 00:03:05 -0600

    Update Code.py

--- Code.py
+++ Code.py
@@ -84,12 +84,14 @@ train_exog = exog[:-4].copy()
 test_exog = exog[-4:].copy()
 
 st.markdown("""
----  
-### 🏪 Adjust Store Count Forecast""")
+<hr>
+<h3 style='text-align: center;'>🏪 Adjust Store Count Forecast</h3>
 
-st.markdown("""
-Before you begin reading the analysis, input the expected store count for the upcoming quarter. This way, you can test different outcomes for future revenue based on your location expectations.
-""")
+<div style='text-align: center; font-size: 16px; margin-bottom: 20px;'>
+Before you begin reading the analysis, input the expected store count for the upcoming quarter.  
+This allows you to test different outcomes for future revenue based on your operational growth assumptions.
+</div>
+""", unsafe_allow_html=True)
 
 col1, col2 = st.columns([1, 3.4])
 
