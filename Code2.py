69b0888b3410c2767b9c32cccce676c9c2963b52
allielind27 <allie.lind@du.edu>
2025-06-03 18:06:10 -0600

    Update Code2.py

--- Code2.py
+++ Code2.py
@@ -1,1 +1,10 @@
+import streamlit as st
 
+st.title("Starbucks Revenue Forecasting App")
+
+st.markdown("""
+Welcome to the Starbucks Revenue Forecasting App!  
+This application provides a powerful tool to forecast Starbucks' quarterly revenue using advanced time-series modeling. 
+By leveraging economic indicators like CPI and operational metrics like store count, the app delivers insights into future revenue trends. 
+Explore interactive visualizations, analyze market sentiment, and benchmark performance against industry peers to make informed decisions.
+""")
